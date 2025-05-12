a2461b40e0b589dfacf67d9928f8163195b60888
Scott <40651157+scottPlusPlus@users.noreply.github.com>
2025-05-12 12:27:32 -0300

    gato api logging

--- src/serverCode/gatoApi.ts
+++ src/serverCode/gatoApi.ts
@@ -1,13 +1,15 @@
 import { loadEnv } from "./server-utils";
 
 export async function loadGatoSheet(): Promise<{ items: Record<string, any>[] }> {
-    const sheetId = loadEnv("NEXT_PUBLIC_GOOGLE_SHEET_ID")!
-    const gatoUrl = new URL(loadEnv("NEXT_PUBLIC_GATO_ENDPOINT")!);
-    const gatoAuth = loadEnv("NEXT_PUBLIC_GATO_AUTH")!
-    gatoUrl.searchParams.set('id', sheetId);
-    gatoUrl.searchParams.set('title', "Sheet1");
     try {
+        const sheetId = loadEnv("NEXT_PUBLIC_GOOGLE_SHEET_ID")!
+        const gatoUrl = new URL(loadEnv("NEXT_PUBLIC_GATO_ENDPOINT")!);
+        const gatoAuth = loadEnv("NEXT_PUBLIC_GATO_AUTH")!
+        gatoUrl.searchParams.set('id', sheetId);
+        gatoUrl.searchParams.set('title', "Sheet1");
+
         const res = await fetch(gatoUrl, { next: { revalidate: 3600 }, headers: { authorization: `Bearer ${gatoAuth}` } });
+        console.log("gato resp: " + res.status);
         const json = await res.json();
         return json;
     } catch (err) {
