c3fd410f3fc5e112fdc4b1433ac3fa2c3e500ece
Scott <40651157+scottPlusPlus@users.noreply.github.com>
2025-05-12 12:12:34 -0300

    error checking for load server data

--- src/serverCode/load-item-infos.ts
+++ src/serverCode/load-item-infos.ts
@@ -4,20 +4,20 @@ import { loadGatoSheet } from "./gatoApi";
 import { ScoutInfo } from "./scoutApi";
 
 export async function loadServerData(): Promise<ItemInfos> {
-
     const sheetData = await loadGatoSheet();
-    const scoutItems: Array<ScoutItem> = sheetData.items.map(x => {
+    const sheetItems = sheetData?.items ?? [];
+    console.log(`loaded ${sheetItems} sheet items`);
+    const scoutItems: Array<ScoutItem> = sheetItems.map(x => {
         const si: ScoutItem = {
             url: x["url"],
             comment: x["comment"] ?? "",
-            tags: (x["tags"] as string).split(",").map(t => t.trim()),
+            tags: ((x["tags"] ?? "") as string).split(",").map(t => t.trim()),
             priority: 0
         };
         return si;
     });
 
-
-    const scoutInfos: Array<ScoutInfo> = sheetData.items.map(x => {
+    const scoutInfos: Array<ScoutInfo> = sheetItems.map(x => {
         const si: ScoutInfo = x["scout"];
         si.url = x.url;
         si.fullUrl = x.url;
@@ -26,10 +26,6 @@ export async function loadServerData(): Promise<ItemInfos> {
         }
         return si;
     });
-    console.log(`loadScoutItems: ${scoutInfos.length} items`);
-    if (scoutInfos.length > 0) {
-        console.log(JSON.stringify(scoutInfos[0]));
-    }
     return {
         items: _.uniqBy(scoutItems, (x) => x.url),
         infos: _.uniqBy(scoutInfos, (x) => x.url)
